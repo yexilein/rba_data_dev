8ff1e3927ecfa5d4a72e1ddee5265fe0e151dbb4
Stephan Fischer <yexilein@hotmail.com>
2018-10-06 22:31:58 -0400

    Use package functions and local files to update medium and efficiencies

--- subtilis/generate_model.py
+++ subtilis/generate_model.py
@@ -8,17 +8,16 @@ import sys
 from os import path
 import math
 
-sys.path = [path.join(sys.path[0], '../..')] + sys.path
+sys.path = [path.join(sys.path[0], '../../rba')] + sys.path
 import rba  # noqa
-
-old_data = 'data/subtilis_ref/old_data/'
+from rba.utils.inject_efficiencies import EfficiencyInjecter  # noqa
 
 
 def main():
-    builder = rba.ModelBuilder('data/subtilis/params.in')
+    builder = rba.ModelBuilder('params.in')
     subtilis = builder.build_model()
-    subtilis.medium = reference_medium(subtilis)
-    add_enzymatic_activities(subtilis.enzymes, subtilis.parameters, 'medium_2')
+    subtilis.set_medium('data/curated_medium.tsv')
+    EfficiencyInjecter(subtilis, 'data/catalytic_activity_medium_2.csv')
     apply_old_stoichiometries(subtilis.enzymes)
     add_flagella_constraint(subtilis)
     subtilis.write()
@@ -65,33 +64,8 @@ def flagella_activation_aggregate():
     return aggregate
 
 
-def add_enzymatic_activities(enzymes, parameters, medium):
-    with open(old_data + 'catalytic_activity.csv', 'r') as f:
-        activity = read_activities(f, medium)
-    for enzyme in enzymes.enzymes:
-        name = enzyme.reaction + '_base_efficiency'
-        parameters.functions.append(
-            rba.xml.Function(name, 'constant',
-                             activity[old_name(enzyme.reaction)])
-        )
-        if is_transport_function(enzyme.forward_efficiency):
-            transport_agg = parameters.aggregates.get_by_id(
-                enzyme.forward_efficiency
-            )
-            transport_agg.function_references[0].function = name
-        else:
-            enzyme.forward_efficiency = name
-        enzyme.backward_efficiency = name
-
-
-def is_transport_function(attribute):
-    return not(
-        attribute in ['default_efficiency', 'default_transporter_efficiency']
-    )
-
-
 def add_zero_cost_flags(enzymes):
-    with open(old_data + 'zero_cost.csv', 'r') as f:
+    with open('data/zero_cost.csv', 'r') as f:
         zero_cost = []
         for line in f:
             zero_cost += line.rstrip('\n').split('\t')
@@ -132,7 +106,7 @@ def old_name(new_name):
 
 
 def apply_old_stoichiometries(enzymes):
-    with open(old_data + 'stoichiometry.csv', 'r') as f:
+    with open('data/stoichiometry.csv', 'r') as f:
         data = {}
         for line in f:
             [species, sto] = line.rstrip('\n').split('\t')
@@ -143,16 +117,6 @@ def apply_old_stoichiometries(enzymes):
                 sr.stoichiometry = data[sr.species]
             except KeyError:
                 pass
-
-
-def reference_medium(subtilis):
-    with open(old_data + 'medium.csv', 'r') as f:
-        old_medium = dict.fromkeys(subtilis.medium, 0)
-        for line in f:
-            met, conc = line.rstrip('\n').split('\t')
-            old_medium[met[:-2]] = conc
-    return old_medium
-
 
 if __name__ == "__main__":
     main()
